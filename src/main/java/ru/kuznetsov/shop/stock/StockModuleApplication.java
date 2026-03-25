51d601ddf7d583eced76837fc40da1f0a386f58b
Pavel Kuznetsov <Technic47@gmail.com>
2026-03-25 10:36:55 +0300

    Small fix

--- src/main/java/ru/kuznetsov/shop/stock/StockModuleApplication.java
+++ src/main/java/ru/kuznetsov/shop/stock/StockModuleApplication.java
@@ -6,11 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import ru.kuznetsov.shop.data.config.SpringConfig;
 import ru.kuznetsov.shop.kafka.config.KafkaConfig;
-import ru.kuznetsov.shop.parameter.config.ParameterConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@Import({SpringConfig.class, ParameterConfig.class, KafkaConfig.class})
+@Import({SpringConfig.class, KafkaConfig.class})
 public class StockModuleApplication {
 
     public static void main(String[] args) {
